a479bfceae4caba5d94dbfca41418c03df6a2339
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2024-07-05 21:27:12 +0300

    Fix docs

--- simcore/src/lib.rs
+++ simcore/src/lib.rs
@@ -1,6 +1,12 @@
+//! SimCore is discrete event simulation framework aimed to provide a solid foundation for building simulation models
+//! of distributed systems. The framework is built around a generic event-driven
+//! [programming model](https://github.com/systems-group/simcore/tree/main/examples/intro) that can be used to model
+//! different domains even beyond distributed systems. It allows to use both callbacks and asynchronous waiting to
+//! conveniently model any execution logic. The versatility of SimCore is demonstrated by using it for building several
+//! [simulation libraries](https://github.com/osukhoroslov/dslab/tree/main/crates) for research and education.
+
 #![warn(missing_docs)]
 #![allow(clippy::needless_doctest_main)]
-#![doc = include_str!("../../readme.md")]
 
 pub mod async_mode;
 pub mod component;
